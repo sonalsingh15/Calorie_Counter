3d0288237752afb19eb221001705b07ae0b158c5
Sonal Singh <sonalsingh@fb.com>
2022-03-05 10:43:13 -0800

    Initial Commit

---
+++ Calorie Counter/Calorie_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calorie_CounterApp.swift
+//  Calorie Counter
+//
+//  Created by Sonal Singh on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Calorie_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calorie Counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calorie Counter
+//
+//  Created by Sonal Singh on 3/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
